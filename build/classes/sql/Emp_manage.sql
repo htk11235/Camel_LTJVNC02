ebfa60c62d250d9398dbbd55471375e90eff9d16
huynh trong khoa <htk11235@gmail.com>
2022-05-25 20:32:32 +0700

    change_name

--- build/classes/sql/Emp_manage.sql
+++ build/classes/sql/Emp_manage.sql
@@ -43,10 +43,39 @@ create table timekeeping(
 	employee_Id int foreign key references employees(employee_Id)
 		on delete cascade
 		on update cascade,
-	day_keeping datetime,
+	day_keeping date,
 	status_ nvarchar(3),
 )
 go
+CREATE or alter TRIGGER addEmployInTimekeeping
+ON employees
+AFTER  insert,delete
+AS
+begin
+	if not exists (select * from deleted)
+		--insert
+		begin
+			DECLARE @cnt INT = 0;
+			WHILE @cnt < 3650
+			BEGIN
+				insert into timekeeping values
+				((select employee_Id from inserted),(select FORMAT( GETDATE()+365*5-@cnt,'yyyy-MM-dd')),'No')
+				SET @cnt = @cnt + 1;
+			END;
+		end;
+	else
+		if not exists (select * from inserted)
+			--delete
+			begin
+				DECLARE @i INT = 0;
+				WHILE @i < 3650
+				BEGIN
+					delete from timekeeping 
+					where employee_Id=(select employee_Id from deleted) and day_keeping=(select FORMAT( GETDATE()+365*5-@i,'yyyy-MM-dd'))
+					SET @i = @i + 1;
+				END;
+			end;		
+end
 GO
 delete from positions
 DBCC CHECKIDENT ('positions', RESEED, 1)
@@ -72,16 +101,10 @@ insert into employees values
 	('Sub acc','1','3','F','12/10/2002','1','000','2')
 go
 
-delete from timekeeping
-DBCC CHECKIDENT ('timekeeping', RESEED, 1)
-insert into timekeeping values
-	('1','9/5/2022','Yes'),
-	('1','10/5/2022','No')
+
 go
 delete from salaries
 DBCC CHECKIDENT ('salaries', RESEED, 1)
 insert into salaries values
 	('1',10,'5','2022')
 
-
-	SELECT * FROM  positions WHERE position_Id = 1
