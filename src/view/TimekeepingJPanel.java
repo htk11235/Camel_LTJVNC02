2d50aee547ae2d69bd141eed59af459f32d640e5
baptr2304 <truongbaophuc456@gmail.com>
2022-05-31 22:45:04 +0700

    bap_u_timekeeping

--- src/view/TimekeepingJPanel.java
+++ src/view/TimekeepingJPanel.java
@@ -153,10 +153,15 @@ public class TimekeepingJPanel extends javax.swing.JPanel {
         jButton3_16.setBackground(new java.awt.Color(237, 74, 74));
         jButton3_16.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         jButton3_16.setForeground(new java.awt.Color(255, 255, 255));
-        jButton3_16.setText("Un Tick");
+        jButton3_16.setLabel(" Un Tick All");
         jButton3_16.setMaximumSize(new java.awt.Dimension(61, 23));
         jButton3_16.setMinimumSize(new java.awt.Dimension(61, 23));
         jButton3_16.setPreferredSize(new java.awt.Dimension(61, 23));
+        jButton3_16.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3_16ActionPerformed(evt);
+            }
+        });
 
         jDateChooser_16.setDateFormatString("dd/MM/yyyy");
         jDateChooser_16.addAncestorListener(new javax.swing.event.AncestorListener() {
@@ -200,6 +205,11 @@ public class TimekeepingJPanel extends javax.swing.JPanel {
         jButton4_16.setMaximumSize(new java.awt.Dimension(61, 23));
         jButton4_16.setMinimumSize(new java.awt.Dimension(61, 23));
         jButton4_16.setPreferredSize(new java.awt.Dimension(61, 23));
+        jButton4_16.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4_16ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -215,15 +225,16 @@ public class TimekeepingJPanel extends javax.swing.JPanel {
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(jLabel2)
+                                .addComponent(jDateChooser_16, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                                 .addComponent(jButton_Save_16, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(jButton4_16, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jButton3_16, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(jLabel2)
-                                .addComponent(jDateChooser_16, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                                .addComponent(jButton3_16, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(11, 11, 11)))))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -244,6 +255,8 @@ public class TimekeepingJPanel extends javax.swing.JPanel {
                     .addComponent(jButton4_16, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(35, Short.MAX_VALUE))
         );
+
+        jButton3_16.getAccessibleContext().setAccessibleName("Un Tick All");
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton_Save_16ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_Save_16ActionPerformed
@@ -305,6 +318,21 @@ public class TimekeepingJPanel extends javax.swing.JPanel {
     private void jDateChooser_16AncestorAdded(javax.swing.event.AncestorEvent evt) {//GEN-FIRST:event_jDateChooser_16AncestorAdded
         // TODO add your handling code here:
     }//GEN-LAST:event_jDateChooser_16AncestorAdded
+
+    private void jButton4_16ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4_16ActionPerformed
+        DefaultTableModel model = (DefaultTableModel) jTable1_16.getModel();
+        for (int i = 0; i < model.getRowCount(); i++) {
+            model.setValueAt(true,i,3);
+        }
+        
+    }//GEN-LAST:event_jButton4_16ActionPerformed
+
+    private void jButton3_16ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3_16ActionPerformed
+        DefaultTableModel model = (DefaultTableModel) jTable1_16.getModel();
+        for (int i = 0; i < model.getRowCount(); i++) {
+            model.setValueAt(false,i,3);
+        }
+    }//GEN-LAST:event_jButton3_16ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
